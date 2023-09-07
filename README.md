816af93e584d870fbde6ccd0ee249edd9178d8a5
Jandrei Timoteo <jandreitimoteo@Jandreis-MacBook-Pro.local>
2023-09-06 19:10:21 -0700

    Fix bug

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # password-generator
+
+# Purpose
+The purpose of a password generator is to create a random, strong and secure passwords to prevent unauthorized individuals to gain access into sensitive data.
+
+# Usage
+1. Navigate to the password generator website.
+2. Click "Generate Password" and the website will generate a secured and random passwords based on your preferences.
+
+# Website
+![Website](image-1.png)
+
+# Credits
+Created by Jandrei Timoteo with the help of Instructor Diarmuid Murphy and Tutor Sheetal Bangalore Srikumar.
+Starter code is retrieved from https://github.com/coding-boot-camp/friendly-parakeet
+
+
+
---
+++ script.js
@@ -0,0 +1,110 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+var uppercaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+var lowercaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+// input and validate
+function generatePasswordChoices() {
+  var passwordChars = '';
+
+  var length = prompt('enter length at least 8 characters and no more than 128 characters')
+  // if length is less than 8 or more than 128 thru an error.
+
+  console.log(length)
+  var lowercase = confirm('Do you want a lower case or no?')
+  console.log(lowercase)
+
+  var uppercase = confirm('Do you want a upper case or no?')
+  console.log(uppercase)
+
+  var numeric = confirm('Do you want a numeric or no?')
+  console.log(numeric)
+
+  var special = confirm('Do you want a special characters?')
+  console.log(special)
+
+  if (lowercase === false && uppercase === false && numeric === false && special === false) {
+    alert('must include at least one character type')
+    return null
+  }
+  var passwordChoices = {
+    length: length,
+    uppercaseChoice: uppercase,
+    lowercaseChoice: lowercase,
+    numericChoice: numeric,
+    specialChoice: special,
+  }
+  return passwordChoices
+}
+function generatePassword() {
+  var choices = generatePasswordChoices();
+  console.log(choices)
+
+  var result = [];
+  var passwordChars = [];
+  var randomChars = [];
+  console.log(choices.uppercaseChoices)
+
+  // group selected characters
+  if (choices.uppercaseChoice) {
+    passwordChars = passwordChars.concat(uppercaseCharacters)
+    randomChars.push(generateRandom(uppercaseCharacters))
+  }
+
+  if (choices.lowercaseChoice) {
+    passwordChars = passwordChars.concat(lowercaseCharacters)
+    randomChars.push(generateRandom(lowercaseCharacters))
+  }
+
+  if (choices.numericChoice) {
+    passwordChars = passwordChars.concat(numericCharacters)
+    randomChars.push(generateRandom(numericCharacters))
+  }
+
+  if (choices.specialChoice) {
+    passwordChars = passwordChars.concat(specialCharacters)
+    randomChars.push(generateRandom(specialCharacters))
+  }
+
+  // pick random cards out of new pool for length of password
+  for (var i = 0; i < choices.length; i++) {
+    console.log(randomChars)
+    var randomCharacter = generateRandom(randomChars);
+    console.log(randomCharacter)
+  }
+  for (var i = 0; i < choices.length; i++) {
+    result[i] = passwordChars[Math.floor(Math.random() * passwordChars.length)]
+    console.log(result)
+    console.log(passwordChars)
+    console.log(randomChars)
+
+
+  }
+  return result.join('')
+}
+
+function generateRandom(arr) {
+  var randomIndex = Math.floor(Math.random() * arr);
+  var randomElement = arr[randomIndex]
+  return randomElement;
+
+
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
